65c6e648ce9c1d16be5062ccf7952f29f25f4260
Jeremias Cardoso <jeremiascardoso00@gmail.com>
2020-06-04 18:51:58 -0300

    Agregar archivos de proyecto.

---
+++ Simon/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Simon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagen_0 = new System.Windows.Forms.PictureBox();
+            this.imagen_1 = new System.Windows.Forms.PictureBox();
+            this.imagen_3 = new System.Windows.Forms.PictureBox();
+            this.imagen_2 = new System.Windows.Forms.PictureBox();
+            this.btn_iniciar = new System.Windows.Forms.Button();
+            this.lbl_puntaje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagen_0
+            // 
+            this.imagen_0.Image = global::Simon.Properties.Resources.button_blue;
+            this.imagen_0.InitialImage = global::Simon.Properties.Resources.button_blue;
+            this.imagen_0.Location = new System.Drawing.Point(47, 41);
+            this.imagen_0.MaximumSize = new System.Drawing.Size(200, 200);
+            this.imagen_0.Name = "imagen_0";
+            this.imagen_0.Size = new System.Drawing.Size(200, 200);
+            this.imagen_0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagen_0.TabIndex = 0;
+            this.imagen_0.TabStop = false;
+            this.imagen_0.Tag = "0";
+            this.imagen_0.Click += new System.EventHandler(this.imagen_0_Click);
+            this.imagen_0.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagen_0_MouseDown);
+            this.imagen_0.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imagen_0_MouseUp);
+            // 
+            // imagen_1
+            // 
+            this.imagen_1.Image = global::Simon.Properties.Resources.button_yellow;
+            this.imagen_1.Location = new System.Drawing.Point(286, 41);
+            this.imagen_1.MaximumSize = new System.Drawing.Size(200, 200);
+            this.imagen_1.Name = "imagen_1";
+            this.imagen_1.Size = new System.Drawing.Size(200, 200);
+            this.imagen_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagen_1.TabIndex = 1;
+            this.imagen_1.TabStop = false;
+            this.imagen_1.Tag = "1";
+            this.imagen_1.Click += new System.EventHandler(this.imagen_1_Click);
+            this.imagen_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagen_1_MouseDown);
+            this.imagen_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imagen_1_MouseUp);
+            // 
+            // imagen_3
+            // 
+            this.imagen_3.Image = global::Simon.Properties.Resources.button_green;
+            this.imagen_3.Location = new System.Drawing.Point(286, 282);
+            this.imagen_3.MaximumSize = new System.Drawing.Size(200, 200);
+            this.imagen_3.Name = "imagen_3";
+            this.imagen_3.Size = new System.Drawing.Size(200, 200);
+            this.imagen_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagen_3.TabIndex = 2;
+            this.imagen_3.TabStop = false;
+            this.imagen_3.Tag = "3";
+            this.imagen_3.Click += new System.EventHandler(this.imagen_3_Click);
+            this.imagen_3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagen_3_MouseDown);
+            this.imagen_3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imagen_3_MouseUp);
+            // 
+            // imagen_2
+            // 
+            this.imagen_2.Image = global::Simon.Properties.Resources.button_red;
+            this.imagen_2.Location = new System.Drawing.Point(47, 282);
+            this.imagen_2.MaximumSize = new System.Drawing.Size(200, 200);
+            this.imagen_2.Name = "imagen_2";
+            this.imagen_2.Size = new System.Drawing.Size(200, 200);
+            this.imagen_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagen_2.TabIndex = 3;
+            this.imagen_2.TabStop = false;
+            this.imagen_2.Tag = "2";
+            this.imagen_2.Click += new System.EventHandler(this.imagen_2_Click);
+            this.imagen_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imagen_2_MouseDown);
+            this.imagen_2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imagen_2_MouseUp);
+            // 
+            // btn_iniciar
+            // 
+            this.btn_iniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_iniciar.Location = new System.Drawing.Point(47, 497);
+            this.btn_iniciar.Name = "btn_iniciar";
+            this.btn_iniciar.Size = new System.Drawing.Size(439, 63);
+            this.btn_iniciar.TabIndex = 4;
+            this.btn_iniciar.Text = "Iniciar Juego";
+            this.btn_iniciar.UseVisualStyleBackColor = true;
+            this.btn_iniciar.Click += new System.EventHandler(this.btn_iniciar_Click);
+            // 
+            // lbl_puntaje
+            // 
+            this.lbl_puntaje.AutoSize = true;
+            this.lbl_puntaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_puntaje.Location = new System.Drawing.Point(248, 244);
+            this.lbl_puntaje.Name = "lbl_puntaje";
+            this.lbl_puntaje.Size = new System.Drawing.Size(37, 37);
+            this.lbl_puntaje.TabIndex = 5;
+            this.lbl_puntaje.Text = "--";
+            this.lbl_puntaje.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 572);
+            this.Controls.Add(this.lbl_puntaje);
+            this.Controls.Add(this.btn_iniciar);
+            this.Controls.Add(this.imagen_2);
+            this.Controls.Add(this.imagen_3);
+            this.Controls.Add(this.imagen_1);
+            this.Controls.Add(this.imagen_0);
+            this.Name = "Form1";
+            this.Text = "Simon dice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen_2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagen_0;
+        private System.Windows.Forms.PictureBox imagen_1;
+        private System.Windows.Forms.PictureBox imagen_3;
+        private System.Windows.Forms.PictureBox imagen_2;
+        private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.Label lbl_puntaje;
+    }
+}
+
---
+++ Simon/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simon
+{
+    public partial class Form1 : Form
+    {
+
+        private int contadorBoton =0;
+        private Random r;
+        private List<int> SimonDice = new List<int>();
+        private List<int> JugadorDice;
+        private int tiempo = 500;
+        private bool enable = false;
+        
+        private int puntaje = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            r = new Random();
+
+        }
+        private void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            lbl_puntaje.Text = "--";
+            this.puntaje = 0;
+            this.contadorBoton = 0;
+            SimonDice = new List<int>();
+            //cargamos la lista
+            SimonDice.Add(r.Next(0, 4));
+            JugadorDice = new List<int>();
+
+
+            //creo un nuevo subproceso para poder usar la funcion sleep
+            //y dormirlo
+            new Thread(IniciarJuego).Start();
+
+            
+
+
+        }
+
+        public void IniciarJuego()
+        {
+            //vi en internet que esto sirve para esperar un tiempo,
+            //en este caso sirve para que las imagenes no se prendan ni bien apretas el boton.
+            //podia escribirlo asi como abajo o podia simplemente importar la biblioteca 
+            //threading y aca abajo poner: Thread.Sleep(tiempo);
+            Thread.Sleep(tiempo);
+
+            foreach (int ParteActiva in SimonDice)
+            {
+
+                switch (ParteActiva)
+                {
+                    case 0:
+                        Console.WriteLine("trampa: 1");
+                        imagen_0.Image = Properties.Resources.button_blue___copia;
+                        Thread.Sleep(tiempo);
+                        imagen_0.Image = Properties.Resources.button_blue;
+
+                        break;
+                    case 1:
+                        Console.WriteLine("trampa: 2");
+                        imagen_1.Image = Properties.Resources.button_yellow___copia;
+                        Thread.Sleep(tiempo);
+                        imagen_1.Image = Properties.Resources.button_yellow;
+                        break;
+                    case 2:
+                        Console.WriteLine("trampa: 3");
+                        imagen_2.Image = Properties.Resources.button_red___copia;
+                        Thread.Sleep(tiempo);
+                        imagen_2.Image = Properties.Resources.button_red;
+                        break;
+                    case 3:
+                        Console.WriteLine("trampa: 4");
+                        imagen_3.Image = Properties.Resources.button_green___copia;
+                        Thread.Sleep(tiempo);
+                        imagen_3.Image = Properties.Resources.button_green;
+                        break;
+                }
+
+            }
+            this.enable = true;
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void imagen_0_Click(object sender, EventArgs e)
+        {
+            if (enable) 
+            {
+                String numeroDeImagen = ((PictureBox)sender).Tag.ToString();
+                Console.WriteLine("boton 1: " + numeroDeImagen);
+
+                JugadorDice.Add(int.Parse(numeroDeImagen));
+                contadorBoton++;
+                Control();
+            }
+            
+        }
+
+        private void imagen_1_Click(object sender, EventArgs e)
+        {
+            if (enable)
+            {
+                String numeroDeImagen = ((PictureBox)sender).Tag.ToString();
+                Console.WriteLine("boton 2");
+                JugadorDice.Add(int.Parse(numeroDeImagen));
+                contadorBoton++;
+                Control();
+            }
+        }
+        private void imagen_2_Click(object sender, EventArgs e)
+        {
+            if (enable)
+            {
+                String numeroDeImagen = ((PictureBox)sender).Tag.ToString();
+                Console.WriteLine("boton 3");
+                JugadorDice.Add(int.Parse(numeroDeImagen));
+                contadorBoton++;
+                Control();
+            }
+        }
+
+        private void imagen_3_Click(object sender, EventArgs e)
+        {
+            if (enable)
+            {
+                String numeroDeImagen = ((PictureBox)sender).Tag.ToString();
+                Console.WriteLine("boton 4");
+                JugadorDice.Add(int.Parse(numeroDeImagen));
+                contadorBoton++;
+                Control();
+            }
+        }
+
+        private void Control()
+        {
+            for(int  i=0; i<contadorBoton; i++)
+            {
+                if (SimonDice[i] != JugadorDice[i])
+                {
+                    
+                    lbl_puntaje.Text = $"Puntaje final: {this.puntaje}";
+                    return;
+                }
+                
+            }
+            if (this.contadorBoton == SimonDice.Count())
+            {
+                this.contadorBoton = 0;
+                this.puntaje++;
+                lbl_puntaje.Text = this.puntaje.ToString();
+                //cargamos la lista
+                SimonDice.Add(r.Next(0, 4));
+                JugadorDice = new List<int>();
+                this.enable = false;
+
+                Thread.Sleep(2000);
+
+                //creo un nuevo subproceso para poder usar la funcion sleep
+                //y dormirlo
+                new Thread(IniciarJuego).Start();
+            }
+            else
+            {
+                return;
+            }
+            
+        }
+
+        private void imagen_0_MouseDown(object sender, MouseEventArgs e)
+        {
+            imagen_0.Image = Properties.Resources.button_blue___copia;
+        }
+
+        private void imagen_0_MouseUp(object sender, MouseEventArgs e)
+        {
+            imagen_0.Image = Properties.Resources.button_blue;
+        }
+
+        private void imagen_1_MouseDown(object sender, MouseEventArgs e)
+        {
+            imagen_1.Image = Properties.Resources.button_yellow___copia;
+        }
+
+        private void imagen_1_MouseUp(object sender, MouseEventArgs e)
+        {
+            imagen_1.Image = Properties.Resources.button_yellow;
+        }
+
+        private void imagen_2_MouseDown(object sender, MouseEventArgs e)
+        {
+            imagen_2.Image = Properties.Resources.button_red___copia;
+        }
+
+        private void imagen_2_MouseUp(object sender, MouseEventArgs e)
+        {
+            imagen_2.Image = Properties.Resources.button_red;
+        }
+
+        private void imagen_3_MouseDown(object sender, MouseEventArgs e)
+        {
+            imagen_3.Image = Properties.Resources.button_green___copia;
+
+        }
+
+        private void imagen_3_MouseUp(object sender, MouseEventArgs e)
+        {
+            imagen_3.Image = Properties.Resources.button_green;
+        }
+
+    }
+}
